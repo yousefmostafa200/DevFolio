3a5f764774e3e2ecd95bcacb202d866d74fc64be
yousefmostafa <yousefmostafadarwish2000@gmail.com>
2024-04-30 00:28:55 +0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+let typed = new Typed('#typing-text', {
+  strings: ['Programmer', 'Web Developer', 'Designer'],
+  typeSpeed: 150,
+  backSpeed: 150,
+  loop: true,
+});
+
+function userScroll() {
+  const navbar = document.querySelector('.navbar');
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+      navbar.classList.add('bg-dark');
+      navbar.classList.add('navbar-sticky');
+    } else {
+      navbar.classList.remove('bg-dark');
+      navbar.classList.remove('navbar-sticky');
+    }
+  });
+}
+
+document.addEventListener('DOMContentLoaded', userScroll);
